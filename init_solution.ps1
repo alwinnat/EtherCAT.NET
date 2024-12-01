c89d2e786c02b69d95235b40b913210b025e7171
Alwin Natesta <20288831+alwinnat@users.noreply.github.com>
2024-12-01 15:06:13 +0100

    Modify 'init_solution.ps1' to run with Visual Studio 2022

--- init_solution.ps1
+++ init_solution.ps1
@@ -7,23 +7,7 @@ $path = "$($PSScriptRoot)/artifacts/bin32"
 New-Item -Force -ItemType directory -Path $path
 Set-Location -Path $path
 
-if ($IsWindows)
-{
-	cmake ./../../native -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "Win32"
-}
-elseif ($IsLinux)
-{
-    cmake ./../../native -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY
-}
-elseif ($IsMacOS)
-{
-    # The i386 architecture is deprecated for macOS
-    throw [System.PlatformNotSupportedException]
-}
-else 
-{
-    throw [System.PlatformNotSupportedException]
-}
+cmake ./../../native -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 17 2022" -A "Win32"
 
 # x64
 Write-Host "Creating native x64 project."
@@ -31,18 +15,7 @@ $path = "$($PSScriptRoot)/artifacts/bin64"
 New-Item -Force -ItemType directory -Path $path
 Set-Location -Path $path
 
-if ($IsWindows)
-{
-	cmake ./../../native -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 16 2019" -A "x64"
-}
-elseif ($IsLinux -or $IsMacOS)
-{
-    cmake ./../../native -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-m64 -DCMAKE_CXX_FLAGS=-m64
-}
-else 
-{
-    throw [System.PlatformNotSupportedException]
-}
+cmake ./../../native -DCMAKE_CONFIGURATION_TYPES:STRING="Debug;Release" -G "Visual Studio 17 2022" -A "x64"
 
 # return
 Set-Location -Path $PSScriptRoot
